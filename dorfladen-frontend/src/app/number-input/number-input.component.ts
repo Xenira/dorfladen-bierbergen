5390b4dd1ff240bcd36f3a8471504455b8242040
Xenira <1288524+Xenira@users.noreply.github.com>
2022-01-03 01:34:12 +0100

    Enforcing min and max values in number control [Resolves #20]

--- dorfladen-frontend/src/app/number-input/number-input.component.ts
+++ dorfladen-frontend/src/app/number-input/number-input.component.ts
@@ -3,17 +3,23 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'dlb-number-input',
   templateUrl: './number-input.component.html',
-  styleUrls: ['./number-input.component.scss']
+  styleUrls: ['./number-input.component.scss'],
 })
 export class NumberInputComponent implements OnInit {
-
   private _value?: number;
   @Input()
   get value(): number | undefined {
     return this._value;
   }
   set value(v: number | undefined) {
-    this._value = v;
+    v = v || 0;
+    if (this.min !== undefined) {
+      v = Math.max(this.min, v);
+    }
+    if (this.max !== undefined) {
+      v = Math.min(this.max, v);
+    }
+    this._value = v || undefined;
     this.valueChange.emit(v);
   }
 
@@ -22,22 +28,11 @@ export class NumberInputComponent implements OnInit {
 
   @Output() valueChange = new EventEmitter<number>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   public add(count: number): void {
-    console.log('add', count)
-    let newValue = (this.value || 0) + count;
-    if (this.min) {
-      newValue = Math.max(this.min, newValue);
-    }
-    if (this.max) {
-      newValue = Math.min(this.max, newValue);
-    }
-    this.value = newValue;
-    this.valueChange.emit(this.value);
+    this.value = (this.value || 0) + count;
   }
-
 }
